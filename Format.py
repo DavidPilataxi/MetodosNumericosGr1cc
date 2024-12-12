d74e94ac07d49d34b2c09a39e6e5f0ca6820f98b
DavidPilataxi <david.pilataxi@epn.edu.ec>
2024-12-11 21:21:29 -0500

    Corrección examen 1bim

---
+++ Format.py
@@ -0,0 +1,22 @@
+#Formateo con isort
+
+import math
+import os
+import random
+import sys
+from datetime import datetime
+
+
+def is_unique(s):
+    s = list(s)
+    s.sort()
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return 0
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    print(is_unique(input()))
